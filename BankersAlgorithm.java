d149b3efed6595b917968eb6bc1f626fdda97a8e
vivrelavie <stefaniegabion07@gmail.com>
2024-11-29 20:52:57 +0800

    feat: enter values for each process
    - enter max and allocation
    - enter process into a list

--- BankersAlgorithm.java
+++ BankersAlgorithm.java
@@ -23,11 +23,35 @@ public class BankersAlgorithm {
             available.add(sc.nextInt());
         }
         
+        List<Process> processes = new ArrayList<>();
+        for(int i = 0; i < processCount; i++){
+            System.out.println("\nEnter data for process " + (i + 1) + ": ");
+
+            List <Integer> max = new ArrayList<>();
+            List <Integer> allocation = new ArrayList<>();
+
+            System.out.println("\nMaximum resource demands:");
+            for(int j = 0; j < resourceCount; j++){
+                char resourceLetter = (char) ('A' + j);
+                System.out.print("Resource " + resourceLetter + ": ");
+                max.add(sc.nextInt());
+            }
+
+            System.out.println("\nAllocated resources:");
+            for(int k = 0; k < resourceCount; k++){
+                char resourceLetter = (char) ('A' + k);
+                System.out.print("Resource " + resourceLetter + ": ");
+                allocation.add(sc.nextInt());
+            }
+
+            processes.add(new Process(i, allocation, max));
+        }
+
         // Validate if deadlock
         // Validate if sequence is safe
 
         // Output table matrix
-        
+
         sc.close();
     }
 
